13b096777c0d0bc9ae2087fc62ade5f899561639
apajadon <aurelien.pajadon.prestataire@lpgsystems.com>
2025-01-16 17:37:05 +0100

    Adding Vee Porting

---
+++ ExtraSource/Cmp/THW/thw_vee.c
@@ -0,0 +1,359 @@
+#include <MEM_Core/VEE/vee_api.h>
+
+/*
+ * hwt_menuMemory.c
+ *
+ *  Created on: 21 Mars. 2023
+ *      Author: apajadon
+ *
+ * Ce sous-composant du composant HWT s'occupe de gérer les test hard concernant
+ * la mémoire Flash.\n
+ * Ces fonctions sont appelées par le composant principal THW.\n\n
+ */
+#ifdef MODE_THW
+
+#include <cmsis_os2.h>
+#include <MEM_Core/EEPROM_Emul/Core/eeprom_emul_types.h>
+#include <MEM_Core/EEPROM_Emul/Core/eeprom_emul.h>
+#include <stddef.h>
+#include <THW/thw.h>
+#include "THW_core/THW_testHardware_common.h"
+
+
+const char thw_drv_vee_menuName[] = "Driver | VEE";
+
+
+//-------------------------
+static void 	thw_drv_vee_writeNPacks(uint16_t packQty);
+static void 	thw_drv_vee_writeAndVerifyNPacks(uint16_t packQty);
+static void 	thw_drv_vee_veeCleanup();
+static void 	thw_drv_vee_veeFormat();
+
+static void 	thw_drv_vee1_write1Pack(void);
+static void 	thw_drv_vee1_write10Pack(void);
+static void 	thw_drv_vee1_writeAndVerify10Pack(void);
+
+
+st_thw_menuItem thw_drv_vee_menuTab[] = {
+		{.name = "VEE1 - Write 1 pack", 			.pActionFn = thw_drv_vee1_write1Pack, 			.info = 0},
+		{.name = "VEE1 - Write 10 packs", 			.pActionFn = thw_drv_vee1_write10Pack, 			.info = 0},
+		{.name = "VEE1 - Write & Verif 10 packs", 	.pActionFn = thw_drv_vee1_writeAndVerify10Pack, .info = 0},
+		{.name = "VEE1 - cleanup", 					.pActionFn = thw_drv_vee_veeCleanup, 			.info = 0},
+		{.name = "VEE1 - format", 					.pActionFn = thw_drv_vee_veeFormat, 			.info = 0},
+};
+uint16_t thw_drv_vee_menuTabSize = sizeof(thw_drv_vee_menuTab) / sizeof(st_thw_menuItem);
+
+#define THW_DRV_VEE_OFFSET_DISPLAY 19
+
+//****************************************
+//	*** MEMORY prototype encapsulation ***
+//****************************************
+
+static void thw_drv_vee_DisplayMenu(void);
+static void thw_drv_vee_ManageChoice(char CodeToManage);
+
+
+
+//------------------------------------------------------------------------------
+// DESCRIPTION:         void thw_drv_vee_setActive(void)
+// PARAMETERS:          void
+// RETURN VALUE:        void
+// DESIGN INFORMATION:
+//------------------------------------------------------------------------------
+void thw_drv_vee_setActive(void)
+{
+	// Menu affiché
+	thw_actualMenu.displayMenu = 	thw_drv_vee_DisplayMenu;
+	thw_actualMenu.manageChoiceFn = thw_drv_vee_ManageChoice;
+	thw_actualMenu.refreshFn = 		NULL;
+
+	// Init MEM
+	if(VEE_init() != vee_error__OK){
+		THW_printf("HWT - Test Memoire - ERROR : VEE_init()\r\n");
+		thw_main_setActive();		// back to previous menu
+	}
+
+}
+
+
+//*****************************************************************************
+// 							LOCAL FUNCTIONS
+//*****************************************************************************
+//********************************
+//	*** TestMemory Main Menu ***
+//********************************
+//------------------------------------------------------------------------------
+/// \fn 		void thw_drv_vee_DisplayMenu(void)
+/// \brief
+//------------------------------------------------------------------------------
+static void thw_drv_vee_DisplayMenu(void)
+{
+	// Affichage Banner
+	THW_Banner(thw_drv_vee_menuName);
+	THW_printf("\r\n");
+
+	THW_printf("                         VEE\r\n");
+	THW_printf("Variable Qty (1 pack)  : %d  \r\n", 			EE_ex_get_variablesQty());
+	THW_printf("Page Active            : %d\t(0x%08X)  \r\n", 	EE_ex_get_activePage(), 		EE_ex_get_activePageAddress());
+	THW_printf("nbMaxElementsByPage    : %d  \r\n", 			EE_ex_get_nbMaxElementsByPage());
+	THW_printf("nbMaxWrittenElements   : %d  \r\n", 			EE_ex_get_nbMaxWrittenElements());
+
+
+	THW_printf("\r\n");
+	THW_printf("\r\n");
+
+	// Affichage du menu
+	THW_displayActionMenu(thw_drv_vee_menuTab, thw_drv_vee_menuTabSize);
+}
+
+//------------------------------------------------------------------------------
+/// \fn 		void thw_drv_vee_ManageChoice(char CodeToManage)
+/// \brief
+//------------------------------------------------------------------------------
+static void thw_drv_vee_ManageChoice(char CodeToManage)
+{
+	// Excute the corresponding action
+	if((CodeToManage > 0) && (CodeToManage <= thw_drv_vee_menuTabSize)){
+		if(thw_drv_vee_menuTab[CodeToManage - 1].pActionFn != NULL)
+			thw_drv_vee_menuTab[CodeToManage - 1].pActionFn();
+	}
+	// Or Return to the previous menu
+	else if(CodeToManage == 0){
+		EE_CleanUp();			// Cleanup
+		thw_main_setActive();	// Return to the previous menu
+	}
+}
+
+
+
+
+//==============================================================================
+//									VEE TESTS
+//==============================================================================
+
+//------------------------------------------------------------------------------
+/// \fn 		void thw_drv_vee1_write1Pack(void)
+/// \brief
+//------------------------------------------------------------------------------
+static void thw_drv_vee1_write1Pack(void)
+{
+	thw_drv_vee_writeNPacks(1);
+}
+
+
+
+//------------------------------------------------------------------------------
+/// \fn 		void thw_drv_vee1_write10Pack(void)
+/// \brief
+//------------------------------------------------------------------------------
+static void thw_drv_vee1_write10Pack(void)
+{
+	thw_drv_vee_writeNPacks(10);
+}
+
+//------------------------------------------------------------------------------
+/// \fn 		void thw_drv_vee1_writeAndVerify10Pack(void)
+/// \brief
+//------------------------------------------------------------------------------
+static void thw_drv_vee1_writeAndVerify10Pack(void)
+{
+	thw_drv_vee_writeAndVerifyNPacks(10);
+}
+
+
+
+
+//------------------------------------------------------------------------------
+/// \fn 		void thw_drv_vee_writeNPacks(uint16_t packQty, bool executeCleanup)
+/// \brief
+//------------------------------------------------------------------------------
+static void thw_drv_vee_writeNPacks(uint16_t packQty)
+{
+	uint32_t 	activePage_old = 0;
+	EE_Status writeStatus = EE_OK;
+	uint32_t tickStart = 0;
+	uint32_t time = 0;
+	bool 		atLeastOneWriteError_flag = false;
+	uint16_t	atLeastOneWriteError_ind = 0;
+	EE_Status	atLeastOneWriteError_statusSav = EE_OK;
+
+
+
+	// Get VEE Page before write
+	activePage_old = EE_ex_get_activePage();
+
+	THW_goto(THW_DRV_VEE_OFFSET_DISPLAY + thw_drv_vee_menuTabSize, 0);
+	THW_clearEndOfScreen();
+
+	// Start time watching
+	tickStart = HAL_GetTick();
+
+	// Write N times Variables (with Cleanup or not)(!!!!!value = item.virtAddrStart + i)
+	//------------------------------------------------------------------------------------
+	if(EE_ex_get_variablesQty() != 0){
+		for(uint16_t pack = 0; pack < packQty; pack++){
+			for(uint16_t i = 0; i < EE_ex_get_variablesQty(); i++){
+				uint16_t virtAdd = i + 1;
+				// Write 32 bits (don't write on 0 address)
+				writeStatus = VEE_write(virtAdd, (virtAdd << 16) + i);
+
+				if(writeStatus != EE_OK) {
+					// Store first Time atLeastOneWriteError Flag & Ind & Status
+					if(atLeastOneWriteError_flag == false){
+						atLeastOneWriteError_flag = true;
+						atLeastOneWriteError_ind = i;
+						atLeastOneWriteError_statusSav = writeStatus;
+					}
+				}
+			}
+		}
+	}
+
+
+	THW_goto(THW_DRV_VEE_OFFSET_DISPLAY + thw_drv_vee_menuTabSize, 0);
+	THW_clearEndOfScreen();
+
+	// Compute time
+	time = (HAL_GetTick() - tickStart);
+
+	// Display writing Error
+	if(atLeastOneWriteError_flag == true){
+		THW_printf(" ==> At Least one error when writting (&:0x%04X) - Status %d\r\n",
+				atLeastOneWriteError_ind,
+				atLeastOneWriteError_statusSav);
+	}
+
+	THW_printf("Write done in %d ms      \r\n", time);
+
+	THW_printf("VEE active Page:  %2d --> %2d      \r\n", activePage_old, EE_ex_get_activePage());
+
+
+	THW_avoidClearScreen();
+
+}
+
+
+//------------------------------------------------------------------------------
+/// \fn 		void thw_drv_vee_writeAndVerifyNPacks(uint16_t packQty, bool executeCleanup)
+/// \brief
+//------------------------------------------------------------------------------
+static void thw_drv_vee_writeAndVerifyNPacks(uint16_t packQty)
+{
+	uint32_t 	activePage_old = 0;
+	EE_Status 	writeStatus = EE_OK;
+	EE_Status 	readStatus = EE_OK;
+	uint32_t	nbOfVariables = 0;
+	uint32_t	valRead = 0;
+
+	nbOfVariables = EE_ex_get_variablesQty();
+	// Get VEE Page before write
+	activePage_old = EE_ex_get_activePage();
+	THW_goto(THW_DRV_VEE_OFFSET_DISPLAY + thw_drv_vee_menuTabSize, 0);
+	THW_clearEndOfScreen();
+	// Write N times Variables (with Cleanup or not)(!!!!!value = item.virtAddrStart + i)
+	//------------------------------------------------------------------------------------
+
+	if(nbOfVariables!= 0)
+	{
+		for(uint16_t pack = 0; pack < packQty; pack++)
+		{
+			// Write Pack
+			for(uint16_t i = 0; i < nbOfVariables; i++)
+			{
+				uint16_t virtAdd = i + 1; // Don't write on 0 address
+				// Write 32 bits
+				writeStatus = VEE_write(virtAdd, (virtAdd << 16) + i);
+				if(writeStatus != EE_OK)
+				{
+					THW_printf(" ==> At Least one error when writting (&:0x%04X) - Status %d\r\n",
+							virtAdd, writeStatus);
+					return;
+				}
+			}
+
+		}
+		for(uint16_t pack = 0; pack < packQty; pack++)
+		{
+			// Read Pack & Compare
+			for(uint16_t i = 0; i < nbOfVariables; i++)
+			{
+				uint16_t virtAdd = i + 1; // don't read on 0 address
+				// Read 32 bits
+				readStatus = VEE_read(virtAdd, &valRead);
+				if(readStatus != EE_OK) {
+					THW_printf(" ==> At Least one error when reading (&:0x%04X) - Status %d\r\n",
+							virtAdd, readStatus);
+					return;
+				}else
+				{
+					// Compare
+					if(valRead != (virtAdd << 16) + i)
+					{
+						THW_printf(" ==> Comparing: ERROR\r\n");
+					}
+				}
+			}
+		}
+	}
+	THW_printf(" ==> Comparing OK\r\n");
+	// Display VEE Page (Old --> New)
+	THW_printf("VEE active Page:  %2d --> %2d      \r\n", activePage_old, EE_ex_get_activePage());
+	THW_avoidClearScreen();
+
+}
+
+
+//------------------------------------------------------------------------------
+/// \fn 		void thw_drv_vee_veeCleanup(void)
+/// \brief
+//------------------------------------------------------------------------------
+static void thw_drv_vee_veeCleanup(void)
+{
+	uint32_t time,tickStart = 0;
+
+	THW_clearEndOfScreen();
+	THW_goto(THW_DRV_VEE_OFFSET_DISPLAY + thw_drv_vee_menuTabSize, 0);
+
+	// Start time watching
+	tickStart = HAL_GetTick();
+
+	// Clean Vee
+	EE_CleanUp();// Compute time
+	time = (HAL_GetTick() - tickStart);
+
+	THW_printf("Cleanup in %d ms      \r\n", time);
+
+	// Display VEE Page (Old --> New)
+	THW_printf("VEE active Page:  %2d      \r\n", EE_ex_get_activePage());
+	THW_avoidClearScreen();
+}
+
+
+//------------------------------------------------------------------------------
+/// \fn 		void thw_drv_vee_veeFormat(void)
+/// \brief
+//------------------------------------------------------------------------------
+static void thw_drv_vee_veeFormat()
+{
+	uint32_t activePage_old = 0;
+	uint32_t time,tickStart = 0 ;
+
+	THW_clearEndOfScreen();
+	THW_goto(THW_DRV_VEE_OFFSET_DISPLAY + thw_drv_vee_menuTabSize, 0);
+
+	// Start time watching
+	tickStart = HAL_GetTick();
+
+	// Format Vee
+	activePage_old = EE_ex_get_activePage();
+	EE_Format(EE_FORCED_ERASE);
+	// Compute time
+	time = (HAL_GetTick() - tickStart);
+
+	THW_printf("Format in %d ms      \r\n", time);
+	THW_printf("VEE active Page:  %2d --> %2d      \r\n", activePage_old, EE_ex_get_activePage());
+	THW_avoidClearScreen();
+
+}
+
+#endif //MODE_THW
---
+++ ExtraSource/subModule/MEM_Core/VEE/vee.c
@@ -0,0 +1,385 @@
+/*
+ * vee.c
+ *
+ *  Created on: 18 avr. 2024
+ *      Author: apajadon
+ */
+#include <cmp.h>
+#include <MEM_Core/EEPROM_Emul/Core/eeprom_emul.h>
+#include <MEM_Core/mem_api.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <MEM_Core/VEE/vee_api.h>
+
+//-----------------------------------------------------------------------------
+// INTERNAL DATA
+//-----------------------------------------------------------------------------
+typedef struct{
+	cmp_mode_t 		cmpStatus;
+	cmp_mode_t 		Vee1_Status;
+	cmp_mode_t 		Vee2_Status;
+	cmp_mode_t 		Vee3_Status;
+} vee_internalData_t;
+vee_internalData_t vee_internalData = {
+		.cmpStatus = cmp_mode_notDefined,
+		.Vee1_Status = cmp_mode_notDefined,
+		.Vee2_Status = cmp_mode_notDefined,
+		.Vee3_Status = cmp_mode_notDefined,
+};
+
+//=====================================================================================================================
+//=====================================================================================================================
+//													VEE - FOPS CONFIGURATION
+//=====================================================================================================================
+//=====================================================================================================================
+
+//VEE1
+mem_vee_confEmul_t mem_vee_confItem_vee1 = {
+		.variableCpt = 				0,
+		.writeVariable32bits = 		EE_WriteVariable32bits,
+		.readVariable32bits = 		EE_ReadVariable32bits,
+		.cleanUp = 					EE_CleanUp,
+		.format = 					EE_Format,
+		.get_variablesQty = 		EE_ex_get_variablesQty,
+		.get_activePage = 			EE_ex_get_activePage,
+		.get_activePageAddress = 	EE_ex_get_activePageAddress,
+		.get_nbMaxElementsByPage = 	EE_ex_get_nbMaxElementsByPage,
+//		.get_pagesQty =				EE_ex_get_pagesQty,
+		.get_nbMaxWrittenElements = EE_ex_get_nbMaxWrittenElements,
+//		.get_startPage = 			EE_ex_get_startPage,
+//		.get_endPage = 				EE_ex_get_endPage,
+//		.get_startEepromAddress = 	EE_ex_get_startEepromAddress,
+//		.get_endEepromAddress = 	EE_ex_get_endEepromAddress,
+};
+
+
+
+
+//=====================================================================================================================
+//=====================================================================================================================
+//													LOCAL FUNCTIONS
+//=====================================================================================================================
+//=====================================================================================================================
+
+//------------------------------------------------------------------------------
+/// \fn 		mem_vee_confEmul_t * vee_getConfigurationInstance(vee_number_e vee_number)
+/// \brief
+//------------------------------------------------------------------------------
+static mem_vee_confEmul_t * vee_getConfigurationInstance()
+{
+	return &mem_vee_confItem_vee1;
+}
+
+//=====================================================================================================================
+//=====================================================================================================================
+//													API FUNCTIONS
+//=====================================================================================================================
+//=====================================================================================================================
+
+
+//------------------------------------------------------------------------------
+/// \fn 		mem_vee_error_e VEE_init(void)
+/// \brief		Initialize component (Os not Running)
+//------------------------------------------------------------------------------
+vee_error_e VEE_init(void)
+{
+
+	// Component Status
+	vee_internalData.cmpStatus = cmp_mode_notDefined;
+	HAL_FLASH_Unlock();
+
+	// Init VEE
+//	EE_Status ret = EE_Init(EE_CONDITIONAL_ERASE);
+	EE_Status ret = EE_Init(EE_FORCED_ERASE);
+	if(ret != EE_OK){
+		EE_Format(EE_FORCED_ERASE);
+		return vee_error__veeInitError;
+	}
+	vee_internalData.Vee1_Status = cmp_mode_nominal;
+	return vee_error__OK;
+}
+
+//------------------------------------------------------------------------------
+/// \fn 		mem_vee_error_e VEE_exit(void)
+/// \brief		Finalize component (Os not Running)
+//------------------------------------------------------------------------------
+vee_error_e VEE_exit(void)
+{
+	// Exit VEE
+	vee_internalData.Vee1_Status = cmp_mode_notDefined;
+
+	HAL_FLASH_Lock();
+	return vee_error__OK;
+}
+
+
+//--------------------------------------------------------------------------------------------------------
+/// \fn 		vee_error_e VEE_write(uint16_t VirtAddress, uint32_t data, vee_number_e vee_number)
+/// \brief		Write to VEE
+//--------------------------------------------------------------------------------------------------------
+vee_error_e VEE_write(uint16_t VirtAddress, uint32_t data)
+{
+	// Get Vee Configuration
+	mem_vee_confEmul_t *pConfEmulToUse = vee_getConfigurationInstance();
+	if(pConfEmulToUse == NULL)
+		return vee_error__VeeNotFound;
+
+	// Write to Vee
+	EE_Status 	status = pConfEmulToUse->writeVariable32bits((uint16_t)VirtAddress, data);
+	if(status == EE_CLEANUP_REQUIRED)
+	{
+		// Execute CleanUp if required
+		status = pConfEmulToUse->cleanUp();
+		if(status == EE_ERROR_NOERASING_PAGE)
+			status = EE_OK;
+	}
+	if(status == EE_OK)
+	{
+		// Increment Variable Qty
+		pConfEmulToUse->variableCpt++;
+		return vee_error__OK;
+	}
+	else
+	{
+		return vee_error__writeError;
+	}
+}
+
+
+//---------------------------------------------------------------------------------------------------------------------
+/// \fn 		vee_error_e VEE_read(uint16_t VirtAddress, uint32_t* data, vee_number_e vee_number)
+/// \brief		Read Data from VEE
+//---------------------------------------------------------------------------------------------------------------------
+vee_error_e VEE_read(uint16_t VirtAddress, uint32_t* pData)
+{
+	// Get Vee Configuration
+	mem_vee_confEmul_t *pConfEmulToUse = vee_getConfigurationInstance();
+	if(pConfEmulToUse == NULL)
+		return vee_error__VeeNotFound;
+
+	// Read from Vee
+	EE_Status 	status = pConfEmulToUse->readVariable32bits((uint16_t)VirtAddress, pData);
+	if(status == EE_CLEANUP_REQUIRED)
+	{
+		// Execute CleanUp if required
+		status = pConfEmulToUse->cleanUp();
+		if(status == EE_ERROR_NOERASING_PAGE)
+			status = EE_OK;
+	}
+
+	if(status == EE_OK) {
+		return vee_error__OK;
+	}
+	else if (status == EE_NO_DATA) {
+		return vee_error__dataNotFound;
+	}
+	else {
+		return vee_error__readError;
+	}
+}
+
+
+//------------------------------------------------------------------------------
+/// \fn 		vee_error_e VEE_format(vee_number_e vee_number)
+/// \brief		Format VEE
+//------------------------------------------------------------------------------
+vee_error_e VEE_format()
+{
+	// Get Vee Configuration
+	mem_vee_confEmul_t *pConfEmulToUse = vee_getConfigurationInstance();
+	if(pConfEmulToUse == NULL)
+		return vee_error__VeeNotFound;
+
+	// Format Vee
+	EE_Status 	status = pConfEmulToUse->format(EE_FORCED_ERASE);
+	if(status == EE_OK)
+	{
+		// Raz Variable Qty
+		pConfEmulToUse->variableCpt = 0;
+		return vee_error__OK;
+	}
+	else
+	{
+		return vee_error__formatError;
+	}
+}
+
+
+//------------------------------------------------------------------------------
+/// \fn 		vee_error_e VEE_cleanUp(vee_number_e vee_number)
+/// \brief		Finalize component
+//------------------------------------------------------------------------------
+vee_error_e VEE_cleanUp()
+{
+	// Get Vee Configuration
+	mem_vee_confEmul_t *pConfEmulToUse = vee_getConfigurationInstance();
+	if(pConfEmulToUse == NULL)
+		return vee_error__VeeNotFound;
+
+	// Cleanup Vee
+	EE_Status 	status = pConfEmulToUse->cleanUp();
+	if(status == EE_OK)
+		return vee_error__OK;
+	else if(status == EE_ERROR_NOERASING_PAGE)
+		return vee_error__OK;
+	else
+		return vee_error__cleanupError;
+}
+
+
+
+//=====================================================================================================================
+//=====================================================================================================================
+//												API FUNCTIONS - VEE INFORMATION
+//=====================================================================================================================
+//=====================================================================================================================
+
+
+//------------------------------------------------------------------------------
+/// \fn 		uint32_t VEE_get_variablesQty(vee_number_e vee_number)
+/// \brief
+//------------------------------------------------------------------------------
+uint32_t VEE_get_variablesQty()
+{
+	uint32_t var = 0;
+	mem_vee_confEmul_t *pConfEmulToUse = vee_getConfigurationInstance();
+
+	if(pConfEmulToUse != NULL)
+		var = pConfEmulToUse->get_variablesQty();
+
+	return var;
+}
+
+//------------------------------------------------------------------------------
+/// \fn 		uint32_t VEE_get_activePage(vee_number_e vee_number)
+/// \brief
+//------------------------------------------------------------------------------
+uint32_t VEE_get_activePage()
+{
+	uint32_t var = 0;
+	mem_vee_confEmul_t *pConfEmulToUse = vee_getConfigurationInstance();
+
+	if(pConfEmulToUse != NULL)
+		var = pConfEmulToUse->get_activePage();
+
+	return var;
+}
+
+//------------------------------------------------------------------------------
+/// \fn 		uint32_t VEE_get_activePageAddress(vee_number_e vee_number)
+/// \brief
+//------------------------------------------------------------------------------
+uint32_t VEE_get_activePageAddress()
+{
+	uint32_t var = 0;
+	mem_vee_confEmul_t *pConfEmulToUse = vee_getConfigurationInstance();
+
+	if(pConfEmulToUse != NULL)
+		var = pConfEmulToUse->get_activePageAddress();
+
+	return var;
+}
+
+//------------------------------------------------------------------------------
+/// \fn 		uint32_t VEE_get_nbMaxElementsByPage(vee_number_e vee_number)
+/// \brief		Finalize component
+//------------------------------------------------------------------------------
+uint32_t VEE_get_nbMaxElementsByPage()
+{
+	uint32_t var = 0;
+	mem_vee_confEmul_t *pConfEmulToUse = vee_getConfigurationInstance();
+
+	if(pConfEmulToUse != NULL)
+		var = pConfEmulToUse->get_nbMaxElementsByPage();
+
+	return var;
+}
+
+//------------------------------------------------------------------------------
+/// \fn 		uint32_t VEE_get_pagesQty(vee_number_e vee_number)
+//------------------------------------------------------------------------------
+uint32_t VEE_get_pagesQty()
+{
+	uint32_t var = 0;
+	mem_vee_confEmul_t *pConfEmulToUse = vee_getConfigurationInstance();
+
+	if(pConfEmulToUse != NULL)
+		var = pConfEmulToUse->get_pagesQty();
+
+	return var;
+}
+
+//------------------------------------------------------------------------------
+/// \fn 		uint32_t VEE_get_nbMaxWrittenElements(vee_number_e vee_number)
+/// \brief
+//------------------------------------------------------------------------------
+uint32_t VEE_get_nbMaxWrittenElements()
+{
+	uint32_t var = 0;
+	mem_vee_confEmul_t *pConfEmulToUse = vee_getConfigurationInstance();
+
+	if(pConfEmulToUse != NULL)
+		var = pConfEmulToUse->get_nbMaxWrittenElements();
+
+	return var;
+}
+
+//------------------------------------------------------------------------------
+/// \fn 		uint32_t VEE_get_startPage(vee_number_e vee_number)
+/// \brief
+//------------------------------------------------------------------------------
+uint32_t VEE_get_startPage()
+{
+	uint32_t var = 0;
+	mem_vee_confEmul_t *pConfEmulToUse = vee_getConfigurationInstance();
+
+	if(pConfEmulToUse != NULL)
+		var = pConfEmulToUse->get_startPage();
+
+	return var;
+}
+
+//------------------------------------------------------------------------------
+/// \fn 		uint32_t VEE_get_endPage(vee_number_e vee_number)
+/// \brief
+//------------------------------------------------------------------------------
+uint32_t VEE_get_endPage()
+{
+	uint32_t var = 0;
+	mem_vee_confEmul_t *pConfEmulToUse = vee_getConfigurationInstance();
+
+	if(pConfEmulToUse != NULL)
+		var = pConfEmulToUse->get_endPage();
+
+	return var;
+}
+
+//------------------------------------------------------------------------------
+/// \fn 		uint32_t VEE_get_startEepromAddress(vee_number_e vee_number)
+/// \brief
+//------------------------------------------------------------------------------
+uint32_t VEE_get_startEepromAddress()
+{
+	uint32_t var = 0;
+	mem_vee_confEmul_t *pConfEmulToUse = vee_getConfigurationInstance();
+
+	if(pConfEmulToUse != NULL)
+		var = pConfEmulToUse->get_startEepromAddress();
+
+	return var;
+}
+
+//------------------------------------------------------------------------------
+/// \fn 		uint32_t VEE_get_endEepromAddress(vee_number_e vee_number)
+/// \brief		Finalize component
+//------------------------------------------------------------------------------
+uint32_t VEE_get_endEepromAddress()
+{
+	uint32_t var = 0;
+	mem_vee_confEmul_t *pConfEmulToUse = vee_getConfigurationInstance();
+
+	if(pConfEmulToUse != NULL)
+		var = pConfEmulToUse->get_endEepromAddress();
+
+	return var;
+}
